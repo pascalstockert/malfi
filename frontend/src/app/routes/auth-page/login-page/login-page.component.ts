23b5463f789c812046a89345d561f69db115f624
pasu <pascal.stockert@live.com>
2024-05-21 22:17:35 +0200

    Add auth to login-page

--- frontend/src/app/routes/auth-page/login-page/login-page.component.ts
+++ frontend/src/app/routes/auth-page/login-page/login-page.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../components/button/button.component';
+import { PageLayoutComponent } from '../../../components/page-layout/page-layout.component';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, PageLayoutComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  constructor(
+    private authService: AuthService,
+  ) {}
 
+  async loginWithGoogle(): Promise<void> {
+    await this.authService.signInWithGoogle();
+  }
 }
